b9a4b1fa09aeeb032bfff5dabd44447766d39bdd
Marc Esquerra <esquerra@gmail.com>
2020-04-13 18:47:34 +0100

    Make pattern matching compatible with the Eq typeclass

--- src/main/scala/jude/plugins/renamer/JudeRenamer.scala
+++ src/main/scala/jude/plugins/renamer/JudeRenamer.scala
@@ -11,9 +11,13 @@ class JudeRenamer(val global: Global) extends Plugin {
 
   val name = "Renamer"
   val description =
-    "renames the == and != so that the default implementations can't be used"
+    "alters the default behaviour of the standard scala compiler"
   val components =
-    List[PluginComponent](AfterParserComponent, AfterPatmatComponent)
+    List[PluginComponent](
+      AfterParserComponent,
+      AfterTyperComponent,
+      AfterPatmatComponent
+    )
 
   def quote(s: String): (String, String) =
     s -> s"'$s'"
@@ -24,9 +28,12 @@ class JudeRenamer(val global: Global) extends Plugin {
   def method(s: String): List[(String, String)] =
     List(quote(s), extern(s))
 
-  private object AfterParserComponent extends RanamingComponent {
+  private object AfterParserComponent
+      extends PluginComponent
+      with TypingTransformers {
+
     val runsAfter = List[String]("parser")
-    val phaseName = "AftterParser" + JudeRenamer.this.name
+    val phaseName = "AfterParser" + JudeRenamer.this.name
 
     val mappings: Map[String, String] = List(
       method("$eq$eq"),
@@ -38,61 +45,58 @@ class JudeRenamer(val global: Global) extends Plugin {
       method("hashCode"),
       method("notify"),
       method("notifyAll"),
+      method("equals"),
       method("wait")
     ).flatten.toMap
-  }
 
-  private object AfterPatmatComponent extends RanamingComponent {
-    val runsAfter = List[String]("patmat")
-    val phaseName = "AftterPatmat" + JudeRenamer.this.name
+    def enabled[T <: NameTreeApi]: PartialFunction[T, Boolean] = {
+      case _ => true
+    }
 
-    val mappings: Map[String, String] = List(
-      method("equals")
-    ).flatten.toMap
-  }
-
-  private abstract class RanamingComponent
-      extends PluginComponent
-      with TypingTransformers {
+    private def doit[T <: NameTreeApi](t: T): Boolean =
+      enabled.isDefinedAt(t) && enabled(t) && mappings.contains(
+        t.name.encodedName.toString
+      )
 
     val global: JudeRenamer.this.global.type = JudeRenamer.this.global
-    val mappings: Map[String, String]
     def newPhase(_prev: Phase) = new JudeRenamerPhase(_prev)
 
     class JudeRenamerTransformer(unit: CompilationUnit)
         extends TypingTransformer(unit) {
-      override def transform(tree: Tree) = tree match {
-        case Select(lhs, TermName(id)) if mappings.contains(id) =>
-          Select(transform(lhs), TermName(mappings(id)))
+      override def transform(tree: Tree) = {
+        tree match {
+          case s @ Select(lhs, TermName(id)) if doit(s) =>
+            Select(transform(lhs), TermName(mappings(id)))
 
-        case Ident(TermName(id)) if mappings.contains(id) =>
-          Ident(TermName(mappings(id)))
-        case DefDef(
-            modifiers,
-            TermName(id),
-            tparams,
-            params,
-            retType,
-            rhs
-            ) if mappings.contains(id) =>
-          DefDef(
-            modifiers,
-            TermName(mappings(id)),
-            tparams,
-            params,
-            retType,
-            transform(rhs)
-          )
-        case _ =>
-          // I'll keep this in here. It's good to run experiments
-          // println(s"""|
-          //   |=================
-          //   |$tree
-          //   |-----------------
-          //   |${showRaw(tree)}
-          //   |=================
-          //   |""".stripMargin)
-          super.transform(tree)
+          case i @ Ident(TermName(id)) if doit(i) =>
+            Ident(TermName(mappings(id)))
+          case dd @ DefDef(
+                modifiers,
+                TermName(id),
+                tparams,
+                params,
+                retType,
+                rhs
+              ) if doit(dd) =>
+            DefDef(
+              modifiers,
+              TermName(mappings(id)),
+              tparams,
+              params,
+              retType,
+              transform(rhs)
+            )
+          case _ =>
+            // I'll keep this in here. It's good to run experiments
+            // println(s"""|
+            //   |=================
+            //   |$tree
+            //   |-----------------
+            //   |${showRaw(tree)}
+            //   |=================
+            //   |""".stripMargin)
+            super.transform(tree)
+        }
       }
     }
 
@@ -105,9 +109,100 @@ class JudeRenamer(val global: Global) extends Plugin {
       override def name = JudeRenamer.this.name
 
       override def apply(unit: CompilationUnit): Unit = {
+        unit.body
+          .filter {
+            case _: DefDef => false
+            case _         => true
+          }
+          .foreach {
+            case tree @ (Select(_, PAT_MAT_EQ) | Ident(PAT_MAT_EQ)) =>
+              global.reporter.error(
+                tree.pos,
+                s"`${PAT_MAT_EQ.decodedName}` is a restricted method name that can only be invoked by the compiler"
+              )
+            case _ =>
+          }
         unit.body = new JudeRenamerTransformer(unit).transform(unit.body)
       }
 
     }
   }
+
+  var prexisting: Set[Tree] = Set.empty
+  private object AfterTyperComponent
+      extends PluginComponent
+      with TypingTransformers {
+    val runsAfter = List[String]("typer")
+    val phaseName = "AfterTyper" + JudeRenamer.this.name
+    val global: JudeRenamer.this.global.type = JudeRenamer.this.global
+
+    class JudeRenamerPhase(prev: Phase) extends StdPhase(prev) {
+
+      type PublicCompilationUnit = CompilationUnit
+      override def name = JudeRenamer.this.name
+
+      override def apply(unit: CompilationUnit): Unit = {
+        unit.body.foreach {
+          case tree @ q"$_.==($_)" =>
+            prexisting += tree
+          case _ =>
+        }
+      }
+
+    }
+    def newPhase(_prev: Phase) = new JudeRenamerPhase(_prev)
+  }
+
+  val PAT_MAT_EQ = TermName("restricted$u0020patternMatchingEquals")
+
+  private object AfterPatmatComponent
+      extends PluginComponent
+      with TypingTransformers {
+    val runsAfter = List[String]("patmat")
+    val phaseName = "AfterPatmat" + JudeRenamer.this.name
+    val global: JudeRenamer.this.global.type = JudeRenamer.this.global
+
+    class JudeRenamerTransformer(unit: CompilationUnit)
+        extends TypingTransformer(unit) {
+      override def transform(tree: Tree) = {
+        if (prexisting.contains(tree)) tree
+        else
+          tree match {
+            case q"$pattern.==($obj)" =>
+              try {
+                val typecheck = localTyper.typedPos(tree.pos) _
+                val typeTree = pattern.tpe.dealiasWiden
+                typecheck(q"$pattern : ${obj.tpe}") // This ensures the case is compatible with the match
+                val typeclassSource =
+                  q"_root_.scala.Predef.implicitly[_root_.jude.Eq[$typeTree]]"
+                val tc = typecheck(typeclassSource)
+                val source =
+                  q"($obj.isInstanceOf[$typeTree] && ($tc.$PAT_MAT_EQ($pattern, $obj).asInstanceOf[_root_.scala.Boolean]))"
+                typecheck(source)
+              } catch {
+                case t: Throwable =>
+                  global.reporter.error(
+                    pattern.pos,
+                    t.getMessage()
+                  )
+                  tree
+              }
+            case _ =>
+              super.transform(tree)
+          }
+      }
+    }
+    class JudeRenamerPhase(prev: Phase) extends StdPhase(prev) {
+
+      type PublicCompilationUnit = CompilationUnit
+      override def name = JudeRenamer.this.name
+
+      override def apply(unit: CompilationUnit): Unit = {
+        unit.body = new JudeRenamerTransformer(unit).transform(unit.body)
+      }
+
+    }
+    def newPhase(_prev: Phase) = new JudeRenamerPhase(_prev)
+  }
+
 }
